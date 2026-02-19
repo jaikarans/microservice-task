62a2afbd4fe8d50bb7da29dd94b5a86a73109587
Jaikarans <jaikrns@gmail.com>
2026-02-19 22:14:28 +0530

    order-service: mock kafka for test

--- order-service/src/test/java/io/github/jaikarans/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/io/github/jaikarans/order_service/OrderServiceApplicationTests.java
@@ -1,12 +1,17 @@
 package io.github.jaikarans.order_service;
 
+import io.github.jaikarans.order_service.api.kafka.producer.OrderProducer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class OrderServiceApplicationTests {
+
+	@MockitoBean
+	OrderProducer orderProducer;
 
 	@Test
 	void contextLoads() {
