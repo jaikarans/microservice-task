3998cb719e500f1f29e39dff5cf986df30625254
Jaikarans <jaikrns@gmail.com>
2026-02-20 14:37:41 +0530

    notification-service: fix tests

--- notification-service/src/test/java/io/github/jaikarans/notification_service/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/io/github/jaikarans/notification_service/NotificationServiceApplicationTests.java
@@ -3,7 +3,10 @@ package io.github.jaikarans.notification_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.mail.username=test@test.com",
+		"spring.mail.password=test"
+})
 class NotificationServiceApplicationTests {
 
 	@Test
